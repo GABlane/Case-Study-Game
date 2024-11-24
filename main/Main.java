1b65c18a224e3c2115c8aa30a01c2f79c4af8644
adrianoalwyn <adrianoalwyn>
2024-11-24 22:32:51 +0800

    try lang po

--- main/Main.java
+++ main/Main.java
@@ -12,6 +12,8 @@ public class Main {
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
+        System.out.println("testing");
+
         window.pack();
 
         window.setLocationRelativeTo(null);
