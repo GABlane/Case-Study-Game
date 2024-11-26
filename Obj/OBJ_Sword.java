2d6fea9bb309105acc270d51cf344ddfb44439c2
GABlane <Jgofiangga>
2024-11-26 20:52:01 +0800

    made the objects to the map, will add more obj in the future.

---
+++ Obj/OBJ_Sword.java
@@ -0,0 +1,16 @@
+package Obj;
+
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class OBJ_Sword extends SuperObj {
+    public OBJ_Sword() {
+        name = "key";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("")); // lalagay pako ng sword ni finn
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
