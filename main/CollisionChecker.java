43ac4f1f199efb8028a53e1348402a4a884fa043
GABlane <Jgofiangga>
2024-12-02 22:41:15 +0800

    added the good graphics

--- main/CollisionChecker.java
+++ main/CollisionChecker.java
@@ -7,7 +7,6 @@ public class CollisionChecker {
 
     public CollisionChecker(GamePanel gp) {
         this.gp = gp;
-
     }
 
     public void checkTile(Entity entity) {
@@ -62,61 +61,62 @@ public class CollisionChecker {
         }
 
     }
-    public int checkObject(Entity entity, boolean player){
-        
+
+    public int checkObject(Entity entity, boolean player) {
+
         int index = 999;
 
-        for(int i = 0; i < gp.obj.length; i++){
-            if(gp.obj[i] != null){
-                //get entity solid area position
+        for (int i = 0; i < gp.obj.length; i++) {
+            if (gp.obj[i] != null) {
+                // get entity solid area position
                 entity.solidArea.x = entity.worldX + entity.solidArea.x;
                 entity.solidArea.y = entity.worldY + entity.solidArea.y;
 
                 gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
                 gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
 
-                switch(entity.direnction){
+                switch (entity.direnction) {
                     case "up":
                         entity.solidArea.y -= entity.speed;
-                        //automatically check if the two rectangles are touching or not
-                        if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-                            if (gp.obj[i].collision ==true) {
-                                entity.collisionOn =true;
+                        // automatically check if the two rectangles are touching or not
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision == true) {
+                                entity.collisionOn = true;
                             }
-                            if(player == true){
+                            if (player == true) {
                                 index = i;
                             }
                         }
                         break;
                     case "down":
                         entity.solidArea.y += entity.speed;
-                        if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-                            if (gp.obj[i].collision ==true) {
-                                entity.collisionOn =true;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision == true) {
+                                entity.collisionOn = true;
                             }
-                            if(player == true){
+                            if (player == true) {
                                 index = i;
                             }
                         }
                         break;
                     case "left":
                         entity.solidArea.x -= entity.speed;
-                        if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-                            if (gp.obj[i].collision ==true) {
-                                entity.collisionOn =true;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision == true) {
+                                entity.collisionOn = true;
                             }
-                            if(player == true){
+                            if (player == true) {
                                 index = i;
                             }
                         }
                         break;
                     case "right":
                         entity.solidArea.x += entity.speed;
-                        if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-                            if (gp.obj[i].collision ==true) {
-                                entity.collisionOn =true;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision == true) {
+                                entity.collisionOn = true;
                             }
-                            if(player == true){
+                            if (player == true) {
                                 index = i;
                             }
                         }
