9728fea6849f244443ebc3cbad7f9ada67e4fd59
GABlane <Jgofiangga>
2024-12-04 10:16:19 +0800

    Changed maps

--- main/AssetSetter.java
+++ main/AssetSetter.java
@@ -17,17 +17,21 @@ public class AssetSetter {
 
     public void setObject() {
 
-        gp.obj[0] = new OBJ_Key();
-        gp.obj[0].worldX = 17 * gp.tileSize;
-        gp.obj[0].worldY = 14 * gp.tileSize;
+        // gp.obj[0] = new OBJ_Key();
+        // gp.obj[0].worldX = 17 * gp.tileSize;
+        // gp.obj[0].worldY = 14 * gp.tileSize;
+
+        gp.obj[5] = new OBJ_Key();
+        gp.obj[5].worldX = 42 * gp.tileSize;
+        gp.obj[5].worldY = 12 * gp.tileSize;
 
         gp.obj[1] = new OBJ_DOOR();
-        gp.obj[1].worldX = 32 * gp.tileSize;
-        gp.obj[1].worldY = 27 * gp.tileSize;
+        gp.obj[1].worldX = 24 * gp.tileSize;
+        gp.obj[1].worldY = 35 * gp.tileSize;
 
         gp.obj[4] = new OBJ_Chest();
-        gp.obj[4].worldX = 32 * gp.tileSize;
-        gp.obj[4].worldY = 32 * gp.tileSize;
+        gp.obj[4].worldX = 26 * gp.tileSize;
+        gp.obj[4].worldY = 35 * gp.tileSize;
 
         gp.obj[2] = new OBJ_Boots();
         gp.obj[2].worldX = 16 * gp.tileSize;
