29a475eb76130de4d755e721f87064c8b5faed14
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2024-12-15 14:04:20 +0800

    commited

--- Entity/Entity.java
+++ Entity/Entity.java
@@ -18,7 +18,7 @@ public class Entity {
     public int worldX, worldY;
     public int speed;
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    // public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
 
     public BufferedImage up1,up2, down1,down2, left1, left2, right1, right2;
     public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2,
